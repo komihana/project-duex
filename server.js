3bcb2a4a1cc5efe4c6d052ffd18df7f6378817dc
Han Seoulo <cu.rogue.coder@gmail.com>
2019-05-01 13:12:06 -0400

    initializing files

---
+++ server.js
@@ -0,0 +1,39 @@
+const express= require ('express');
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+const db = require("./models")
+const path = require('path')
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(express.static("public"));
+app.use('/static', express.static('public'))
+app.use('/node_modules', express.static(path.join(__dirname,'node_modules')));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//handlebars
+const exphbs = require ('express-handlebars')
+app.set('views', "public/views/")
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var handlebars = require('handlebars');
+handlebars.registerHelper('moment', require('helper-moment'));
+
+// Routes
+require("")(app);
+require("")(app);
+require("")(app);
+require("")(app);
+
+
+
+db.sequelize.sync().then(function() {
+    app.listen(port, function() {
+      console.log(`Running server on port ${port}`);
+    });
+  });
