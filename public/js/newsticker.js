d27ff227a57794c556eb233717cc234aafcbe8a7
Han Seoulo <cu.rogue.coder@gmail.com>
2019-05-03 00:51:18 -0400

    finished newsticker betches

--- public/js/newsticker.js
+++ public/js/newsticker.js
@@ -5,34 +5,57 @@ $(document).ready(function () {
     let newsArray = [];
 
 
-    var NewsArticle = function (author1, description1, title1, publishedAt1) {
+    var NewsArticle = function (author1, description1, title1, publishedAt1,url1) {
         this.author = author1,
             this.description = description1,
             this.title = title1,
             this.publishedAt = publishedAt1;
+            this.url = url1;
         // console.log(this);
-        return this;
+        // return this;
+        
     };
 
-
-
-
+    
     $.get(newsUrl, function (data) {
-        for (i in data) {
-            for (var article in data[i]) {
-                var newsArticle = new NewsArticle (
-                    data[i][article].author,
-                    data[i][article].description,
-                    data[i][article].title,
-                    data[i][article].publishedAt
+
+        console.log(data.articles);
+
+        let results = data.articles;
+
+        for (var i=0; i < results.length; i++) {
+            var articleTitle = results[i].title;
+            var articleUrl = results[i].url;
+
+            $(".articleLister").append(
+                `<span style='color:red; font-size: 2em; margin-right:1.25em; display:inline-block;'>"<a href="${articleUrl}">${articleTitle}.</a>"</span>`
                 );
-                newsArray.push(newsArticle);
-            }
-            
+
         }
+
+    //     for (i in data) {
+    //         for (var article in data[i]) {
+    //             var newsArticle = new NewsArticle (
+    //                 data[i][article].author,
+    //                 data[i][article].description,
+    //                 data[i][article].title,
+    //                 data[i][article].publishedAt,
+    //                 data[i][article].url
+                    
+                    
+    //             );
+    //             newsArray.push(newsArticle);
+    //         }
+            
+    //     }
         
+
         
-        console.log(newsArray);
-        console.log(newsArticle);
-    });
-});
+    //     console.log(newsArray);
+    //     console.log(newsArticle);
+    // });
+
+
+
+})
+})
